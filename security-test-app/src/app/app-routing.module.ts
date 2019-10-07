b683b0b96f9a6bad94d07f7de83ae123c9874a86
Stefun Hawkins <stefunhawkins@Stefuns-MacBook-Pro.local>
2019-10-07 12:27:53 -0400

    add User service implementation

--- security-test-app/src/app/app-routing.module.ts
+++ security-test-app/src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { UnauthorizedComponent } from './components/unauthorized/unauthorized.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'', redirectTo:'login', pathMatch:'full'},
+  {path:'login', component: LoginComponent},
+  {path:'register', component: RegisterComponent},
+  {path:'profile', component: ProfileComponent},
+  {path:'detail/:id', component: DetailComponent},
+  {path:'admin', component: AdminComponent},
+  {path:'404', component: NotFoundComponent},
+  {path:'401', component: UnauthorizedComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- security-test-app/src/app/app.module.ts
+++ security-test-app/src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { UnauthorizedComponent } from './components/unauthorized/unauthorized.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    DetailComponent,
+    AdminComponent,
+    NotFoundComponent,
+    UnauthorizedComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
